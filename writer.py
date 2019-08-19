e18f7d662d2eb0c2b8e3f550f98167a3e18f1f96
adkrueger <adkrueger@wpi.edu>
2019-08-19 11:14:35 -0400

    Removed unnecessary code.

--- writer.py
+++ writer.py
@@ -58,13 +58,10 @@ def add_dates(start_end, write_sheet, date_range, doc_sheet, bold):
     write_sheet['B1'].value = 'DAY'
     write_sheet['B1'].font = bold
     for i in range(date_range):  # number of readings in a year
-        try:
-            print(doc_sheet['A' + str(i + start)].value)
-            curr_date = doc_sheet['A' + str(i + start)].value.strftime('%m/%d/%Y').lstrip('0').replace('/0', '/')
-            write_sheet['A' + str(i + 2)].value = datetime.strptime(curr_date, '%m/%d/%Y').date()
-            write_sheet['B' + str(i + 2)].value = doc_sheet['B' + str(i + start)].value
-        except AttributeError:
-            pass
+        print(doc_sheet['A' + str(i + start)].value)
+        curr_date = doc_sheet['A' + str(i + start)].value.strftime('%m/%d/%Y').lstrip('0').replace('/0', '/')
+        write_sheet['A' + str(i + 2)].value = datetime.strptime(curr_date, '%m/%d/%Y').date()
+        write_sheet['B' + str(i + 2)].value = doc_sheet['B' + str(i + start)].value
 
 
 def compile_data_to_sheet(write_sheet, sheet_num, sheet_name, num_sheets, doc_wb, year, data_types):
